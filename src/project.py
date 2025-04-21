f736028c51f496193306e78b6183371e44f39e65
MJ Ly <mjl220006@utdallas.edu>
2025-04-20 23:15:48 -0500

    fixed typos for jumping and ducking

--- src/project.py
+++ src/project.py
@@ -10,7 +10,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 running = [pygame.image.load(os.path.join("baseAssets", "DinoRun1.png")),
            pygame.image.load(os.path.join("baseAssets", "DinoRun2.png"))]
 ducking = [pygame.image.load(os.path.join("baseAssets", "DinoDuck1.png")),
-           [pygame.image.load(os.path.join("baseAssets", "DinoDuck2.png"))]]
+           pygame.image.load(os.path.join("baseAssets", "DinoDuck2.png"))]
 jumping = [pygame.image.load(os.path.join("baseAssets", "DinoJump.png"))]
 
 s_cactus = [pygame.image.load(os.path.join("baseAssets", "SmallCactus1.png"))]
@@ -25,6 +25,8 @@ track = [pygame.image.load(os.path.join("baseAssets", "Track.png"))]
 class StarRanger:
     x_pos = 80
     y_pos = 310
+    y_pos_duck = 340
+    JUMP_VEL = 8.5 #velocity
 
     def __init__(self):
         self.run_img = running
@@ -36,6 +38,7 @@ class StarRanger:
         self.player_jump = False
         
         self.step_index = 0 #animateassist
+        self.jump_vel = self.JUMP_VEL
         self.image = self.run_img[0] #startsprite
         self.player_rect = self.image.get_rect() #hitbox
         self.player_rect.x = self.x_pos
@@ -62,7 +65,7 @@ class StarRanger:
             self.player_jump = False
         elif not (self.player_jump or userInput [pygame.K_DOWN]):
             self.player_run = True
-            self.player_suck = False
+            self.player_duck = False
             self.player_jump = False
 
     def run(self):
@@ -73,10 +76,20 @@ class StarRanger:
         self.step_index += 1
 
     def duck(self):
-        pass
+        self.image = self.duck_img[self.step_index//5]
+        self.player_rect = self.image.get_rect()
+        self.player_rect.x = self.x_pos
+        self.player_rect.y = self.y_pos_duck
+        self.step_index += 1
 
     def jump(self):
-        pass
+        self.image = self.jump_img[0]
+        if self.player_jump:
+            self.player_rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8
+        if self.jump_vel < - self.JUMP_VEL:
+            self.player_jump = False
+            self.jump_vel = self.JUMP_VEL
 
     def draw(self, screen):
         screen.blit(self.image, (self.player_rect.x, self.player_rect.y))
