e6fc3c76107de2130524467325e12d792041cdcc
MJ Ly <mjl220006@utdallas.edu>
2025-04-20 22:51:34 -0500

    successful first setup

--- src/project.py
+++ src/project.py
@@ -16,7 +16,7 @@ jumping = [pygame.image.load(os.path.join("baseAssets", "DinoJump.png"))]
 s_cactus = [pygame.image.load(os.path.join("baseAssets", "SmallCactus1.png"))]
 l_cactus = [pygame.image.load(os.path.join("baseAssets", "LargeCactus1.png"))]
 
-bird = [pygame.image.load(os.path.join("baseAssets", "Bird1.png"))]
+flyingMan = [pygame.image.load(os.path.join("baseAssets", "Bird1.png"))]
 
 cloud = [pygame.image.load(os.path.join("baseAssets", "Cloud.png"))]
 
@@ -31,48 +31,55 @@ class StarRanger:
         self.duck_img = ducking
         self.jump_img = jumping
         #gamestart
-        self.dino_run = True
-        self.dino_duck = False
-        self.dino_jump = False
+        self.player_run = True
+        self.player_duck = False
+        self.player_jump = False
         
         self.step_index = 0 #animateassist
         self.image = self.run_img[0] #startsprite
-        self.dino_rect = self.image.get_rect() #hitbox
-        self.dino_rect.x = self.x_pos
-        self.dino_rect.y = self.y_pos
+        self.player_rect = self.image.get_rect() #hitbox
+        self.player_rect.x = self.x_pos
+        self.player_rect.y = self.y_pos
 
     def update(self, userInput):
-        if self.dino_run:
+        if self.player_run:
             self.run()
-        if self.dino_duck:
+        if self.player_duck:
             self.duck()
-        if self.dino_jump:
+        if self.player_jump:
             self.jump()
 
         if self.step_index >= 10:
             self.step_index = 0
 
-        if userInput[pygame.K_UP] and not self.dino_jump:
-            self.dino_run = False
-            self.dino_duck = False
-            self.dino_jump = True
-        elif userInput[pygame.K_DOWN] and not self.dino_jump:
-            self.dino_run = False
-            self.dino_duck = True
-            self.dino_jump = False
-        elif not (self.dino_jump or userInput [pygame.K_DOWN]):
-            self.dino_run = True
-            self.dino_suck = False
-            self.dino_jump = False
+        if userInput[pygame.K_UP] and not self.player_jump:
+            self.player_run = False
+            self.player_duck = False
+            self.player_jump = True
+        elif userInput[pygame.K_DOWN] and not self.player_jump:
+            self.player_run = False
+            self.player_duck = True
+            self.player_jump = False
+        elif not (self.player_jump or userInput [pygame.K_DOWN]):
+            self.player_run = True
+            self.player_suck = False
+            self.player_jump = False
 
     def run(self):
-        pass
+        self.image = self.run_img[self.step_index//5]
+        self.player_rect = self.image.get_rect()
+        self.player_rect.x = self.x_pos
+        self.player_rect.y = self.y_pos
+        self.step_index += 1
 
     def duck(self):
         pass
 
     def jump(self):
         pass
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.player_rect.x, self.player_rect.y))
 
 def main():
     run = True
